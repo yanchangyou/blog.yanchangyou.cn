a5317fe829506f17e2ce3922c06349625f892eed
yanchangyou <yanchangyou@gmail.com>
2014-04-03 23:49:36 +0800

    重构代码

---
+++ poem/js/poem.js
@@ -0,0 +1,77 @@
+// 触屏支持  begin
+if (is_touch_device()) {
+	$(".poem-left,.poem-right").hide();
+	$(".poem").on("swipeleft", function() {
+		showRight();
+	});
+	$(".poem").on("swiperight", function() {
+		showLeft();
+	});
+}
+// 触屏支持 end
+
+var poem_index = 0;
+var poems = [];
+$.ajax({
+	url : "/poem/data/poem.json",
+	success : function(result) {
+		poems = result;
+		poem_index = Math.round(poems.length / 2);
+		showAll();
+
+		if (!is_touch_device()) {
+			$(".poem-left").fadeTo(2000, 0);
+			$(".poem-right").fadeTo(2000, 0);
+		}
+	}
+});
+
+function showAll() {
+	if (0 < poem_index && poem_index < poems.length - 2) {
+		show(poems[poem_index - 1], "poem-left");
+		show(poems[poem_index], "poem");
+		show(poems[poem_index + 1], "poem-right");
+	}
+}
+
+function show(poem, position) {
+	$("." + position + " .title").text(poem.title);
+	$("." + position + " .s1").text(poem.s1);
+	$("." + position + " .s2").text(poem.s2);
+	$("." + position + " .s3").text(poem.s3);
+	$("." + position + " .s4").text(poem.s4);
+	$("." + position + " .date").text("（" + poem.date + "）");
+}
+
+function showLeft() {
+	if (poem_index > 0) {
+		poem_index--;
+	}
+	$(".poem").fadeTo(1000, 0.1, showAll);
+	$(".poem").fadeTo(1000, 1);
+}
+function showRight() {
+	if (poem_index < poems.length) {
+		poem_index++;
+	}
+	$(".poem").fadeTo(1000, 0.1, showAll);
+	$(".poem").fadeTo(1000, 1);
+}
+
+$(".poem-left").click(function() {
+	showLeft();
+});
+$(".poem-right").click(function() {
+	showRight();
+});
+
+$(".poem-left").hover(function() {
+	$(".poem-left").fadeTo(1000, 1);
+}, function() {
+	$(".poem-left").fadeTo(1000, 0);
+});
+$(".poem-right").hover(function() {
+	$(".poem-right").fadeTo(1000, 1);
+}, function() {
+	$(".poem-right").fadeTo(1000, 0);
+});
